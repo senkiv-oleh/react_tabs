d1aa8e3db577694314236e95d33637db636e7757
Oleh Senkiv <o.senkiv.v@gmail.com>
2021-05-31 11:12:18 +0300

    Updates

---
+++ precache-manifest.9bbe6526cc29d866ef44abb677c6a75c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5166b1e966e56e1bf10c34beca1a43a6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "38799915b5ddf9a707f7",
+    "url": "./static/css/main.d3f42b1d.chunk.css"
+  },
+  {
+    "revision": "f4a6e332316f4a7ffe8f",
+    "url": "./static/js/2.50763e96.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.50763e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38799915b5ddf9a707f7",
+    "url": "./static/js/main.b7d5e7f0.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
